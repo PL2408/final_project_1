bb1a0008193669f0bcd808704d72978607538121
Lopihara <pt.lazarenko@gmail.com>
2023-01-03 13:40:13 +0200

    add provider recourse

--- cicd/iac/backend.tf
+++ cicd/iac/backend.tf
@@ -1,4 +1,20 @@
+provider "aws" {
+  region = "eu-central-1"
+  default_tags {
+    tags = {
+      Environment = "DEV"
+      Project        = "Final project 01"
+    }
+  }
+}
+
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.48"
+    }
+  }
   backend "s3" {
     bucket         = "lopihara"
     key            = "terraform_final_project_1_state"
