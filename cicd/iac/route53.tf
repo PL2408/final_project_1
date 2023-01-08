632b3acef3f728dd5fc12d447061919df8dbfd4b
Lopihara <pt.lazarenko@gmail.com>
2023-01-08 12:40:03 +0200

    fix dns record for load balancer

--- cicd/iac/route53.tf
+++ cicd/iac/route53.tf
@@ -1,1 +1,11 @@
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.visitka_hz.id
+  name    = ""
+  type    = "A"
 
+  alias {
+    name                   = aws_lb.fp01_alb.dns_name
+    zone_id                = aws_lb.fp01_alb.zone_id
+    evaluate_target_health = true
+  }
+}
