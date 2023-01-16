54f9adc6e15041907c8ad2aa4a6602621a9c7767
Lopihara <pt.lazarenko@gmail.com>
2023-01-16 16:13:09 +0200

    create ssl certificate

--- cicd/iac/ssl.tf
+++ cicd/iac/ssl.tf
@@ -10,3 +10,8 @@ resource "aws_acm_certificate" "cert" {
     create_before_destroy = true
   }
 }
+
+resource "aws_acm_certificate_validation" "validation_cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation_cert : record.fqdn]
+}
