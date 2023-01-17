89ed4c71a7ac7ed68044523f26f3329f33dea7ea
Lopihara <pt.lazarenko@gmail.com>
2023-01-17 12:47:11 +0200

    create web_server security group

--- cicd/iac/ec2_sg.tf
+++ cicd/iac/ec2_sg.tf
@@ -47,16 +47,55 @@ resource "aws_security_group" "jenkins_agent" {
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.jenkins_server.id]
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-    tags = {
-      Name = "final_prj_01_agent_sg"
-    }
+  tags = {
+    Name = "final_prj_01_agent_sg"
+  }
+}
+
+resource "aws_security_group" "web_server_sg" {
+  name        = "web_ec2_sg"
+  description = "Access to web server"
+  vpc_id      = aws_vpc.fp01_vpc.id
+
+  ingress {
+    description = "SSH traffic from my workstation"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  ingress {
+    description     = "SSH traffic from jenkins_agent_sg"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jenkins_agent.id]
+  }
+  ingress {
+    description     = "HTTP traffic from ELB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.fp01_alb_sg.id]
+  }
+  egress {
+    description = "Outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "final_prj_01_web_server_sg"
   }
 }
