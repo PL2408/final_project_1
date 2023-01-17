25504387b582f2dd1d0006d56b205fe61fcf682a
Lopihara <pt.lazarenko@gmail.com>
2023-01-17 12:45:57 +0200

    add comments to commands

--- cicd/iac/config/user_data_jns.sh
+++ cicd/iac/config/user_data_jns.sh
@@ -4,14 +4,10 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 yum update -y
 yum install java git -y
 
-# jenkins repo
+# setup jenkins repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+# download jenkins
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
-#instal/start jenkins
-yum install jenkins -y
-systemctl enable jenkins
-systemctl start jenkins
 
 
 # swap file for ec2 (5GB)
@@ -24,5 +20,13 @@ swapon /swapfile
 echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
 echo "------------------ SWAP File Created ----------------"
 
-# custom ec2
+
+# install jenkins
+yum install jenkins -y
+# enable auto start
+systemctl enable jenkins
+# start jenkins
+systemctl start jenkins
+
+# custom PS1
 printf 'export PS1="\[\e[0;38;5;216m\]\u\[\e[0m\]@\[\e[0;38;5;39m\]jenkins-sever\[\e[0m\][\[\e[0m\]\w\[\e[0m\]]\[\e[0;38;5;240m\]<\[\e[0;38;5;242m\]\t\[\e[0;38;5;241m\]>\[\e[0m\]:\[\e[0m\] "' >> /etc/bashrc
