6986bff62fb139a6820d300aa8dc94f406f8d960
Lopihara <pt.lazarenko@gmail.com>
2023-01-17 13:46:03 +0200

    create ec2 web server

--- cicd/iac/ec2.tf
+++ cicd/iac/ec2.tf
@@ -55,3 +55,31 @@ resource "aws_instance" "jenkins_agent" {
 output "jenkins_public_ip_01" {
   value = aws_instance.jenkins_agent.public_ip
 }
+
+#--------------------web_server------------------
+resource "aws_instance" "web_server" {
+  ami                         = data.aws_ami.amazon-linux-2.id
+  instance_type               = "t3.micro"
+  key_name                    = "p_l"
+  subnet_id                   = aws_subnet.fp01_pub_sb01.id
+  vpc_security_group_ids      = [aws_security_group.web_server_sg.id]
+  user_data                   = file("config/user_data_web.sh")
+  associate_public_ip_address = true
+  user_data_replace_on_change = true
+  private_ip                  = "10.70.55.157"
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = 20
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "web_server"
+  }
+}
+
+output "web_public_ip" {
+  value = aws_instance.web_server.public_ip
+}
