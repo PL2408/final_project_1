651075ea2f8678f52f56f27418122e9d91487c25
Lopihara <pt.lazarenko@gmail.com>
2023-01-18 10:40:24 +0200

    add 443 HTTPS port

--- cicd/iac/lb_sg.tf
+++ cicd/iac/lb_sg.tf
@@ -9,7 +9,12 @@ resource "aws_security_group" "fp01_alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # later add 443 port
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
