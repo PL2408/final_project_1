c2b7c09e74903c86dec1c31ea3c209218b377f90
Lopihara <pt.lazarenko@gmail.com>
2023-02-21 20:02:05 +0200

    refactoring user_data

--- cicd/iac/config/user_data_jns.sh
+++ cicd/iac/config/user_data_jns.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+# Update Route53 record with new public IP
+aws s3 cp s3://lopihara/config/update_route53.json /opt/
+aws s3 cp s3://lopihara/config/update_route53.sh /opt/
+chmod +x /opt/update_route53.sh
+sed -i 's/HOSTNAME/jenkins/g' /opt/update_route53.json
+/opt/update_route53.sh
+
+# Create crontab job to update record on restart
+cronjob="@reboot /opt/update_route53.sh"
+cat <(echo "$cronjob") | crontab -
+
 yum update -y
 yum install java git docker -y
 
