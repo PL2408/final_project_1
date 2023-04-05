f609a784d6ef37e4d2c04c986a76998ffac24e07
Lopihara <pt.lazarenko@gmail.com>
2023-04-05 19:08:50 +0300

    add healthcheck to tg

--- cicd/iac/tg.tf
+++ cicd/iac/tg.tf
@@ -3,6 +3,12 @@ resource "aws_lb_target_group" "fp01_tg" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = aws_vpc.fp01_vpc.id
+  health_check {
+    path     = "/login"
+    port     = "traffic-port"
+    interval = 30
+    timeout  = 10
+  }
 }
 
 resource "aws_alb_target_group_attachment" "jenkins_tga" {
@@ -17,8 +23,13 @@ resource "aws_lb_target_group" "fp01_web_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.fp01_vpc.id
+  health_check {
+      path     = "/login"
+      port     = "traffic-port"
+      interval = 30
+      timeout  = 10
+  }
 }
-
 resource "aws_alb_target_group_attachment" "web_tga" {
   target_group_arn = aws_lb_target_group.fp01_web_tg.arn
   target_id        = aws_instance.web_server.id
